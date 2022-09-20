392d1d5759c62583a0b6d5e915183f3400be4760
JimmyNeutronDance <14wongom@gmail.com>
2022-09-21 02:13:44 +1000

    Store guesses function add

--- anagram.py
+++ anagram.py
@@ -47,13 +47,13 @@ class Anagram:
 ## Function: Calculate CPU's length of word with word list.
     def cpu_score_calculator(self, final_list):
         if self.difficulty == 'Easy':
-            cpu_word = final_list[round(len(final_list) * 0.25)]
+            cpu_word = final_list[int(round(len(final_list) * 0.25))]
             cpu_score = len(cpu_word)
         elif self.difficulty == 'Medium':
-            cpu_word = final_list[round(len(final_list) * 0.5)]
+            cpu_word = final_list[int(round(len(final_list) * 0.5))]
             cpu_score = len(cpu_word)
         elif self.difficulty == 'Hard':
-            cpu_word = final_list[round(len(final_list) * 0.8)]
+            cpu_word = final_list[int(round(len(final_list) * 0.8))]
             cpu_score = len(cpu_word)
         return cpu_word, cpu_score
 
@@ -67,6 +67,27 @@ class Anagram:
 # Function: Check word against english language
 
 # Function: Store input 3 times, tell user 3 guesses.
+    def store_guesses(self):
+        guesses_remaining = 3
+        guessed_words = []
+        try:
+            while guesses_remaining > 0:
+                user_input = input("Enter your word: ")
+                # TODO: Change conditional to reflect the selected letters on top of valid word.
+                if user_input in english_words_set:
+                    guesses_remaining -= 1
+                    guessed_words.append(user_input)
+                    print(f"'{user_input}' is a valid word for a score of {len(user_input)}. {guesses_remaining} guesses remaining.")
+                elif user_input == '':
+                    break
+                else:
+                    guesses_remaining -= 1
+                    print(f'This word is not in the dictionary. {guesses_remaining} guesses remaining.')
+            
+            return max(guessed_words, key = len)
+        except ValueError:
+            print("Error: You haven't entered any words.")
+            return self.store_guesses()
 
 ## Final Screen "You won/lost/drew _ to _ against the CPU"
     def final_result(self):
@@ -91,5 +112,5 @@ class Anagram:
             possible_words = self.possible_word_list(letters)
             cpu_word, cpu_score = self.cpu_score_calculator(possible_words)
             longest_word_length, longest_word = self.hint_longest_word(possible_words)
-            print(f"The computer's word was {cpu_word} for a score of {cpu_score} points.")
+            print(f"The computer's word was '{cpu_word}' for a score of {cpu_score} points.")
     
