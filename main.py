2942f66f249ee82aa05cf09dac1e1a1bb10e35a7
JimmyNeutronDance <14wongom@gmail.com>
2022-09-21 23:12:57 +1000

    Rules and Quit functioning

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from english_words import english_words_set
 from helper import pick_difficulty, start_menu
 from letter_frequency import choose_consonant, choose_vowel
 from anagram import Anagram
-
-option = start_menu()
-if option == 'Play':
-    #difficulty = pick_difficulty()
-    game = Anagram(1, pick_difficulty())
-    game.start()
-
+import os
+import sys
+while True:
+    option = start_menu()
+    if option == 'Play':
+        game = Anagram(5, pick_difficulty())
+        game.start()
+    elif option == 'Rules':
+        print("The rules are ...")
+        input("Press Enter to return to main menu. ")
+        os.system('clear')
+        continue
+    elif option == 'Quit':
+        sys.exit()
