b67387cf896cdfa188f92900c849dac93f1d6bbc
JimmyNeutronDance <14wongom@gmail.com>
2022-09-22 01:29:40 +1000

    End game fixes

--- anagram.py
+++ anagram.py
@@ -88,7 +88,7 @@ class Anagram:
     def store_guesses(self, letters):
         guesses_remaining = 3
         guessed_words = []
-            while guesses_remaining > 0:
+        while guesses_remaining > 0:
                 user_input = input("Enter your word: ")
                 if self.validate_word(user_input, letters):
                     guesses_remaining -= 1
@@ -100,11 +100,11 @@ class Anagram:
                     guesses_remaining -= 1
                     # TODO FIX ERROR HANDLING
                     print(f'{user_input} cannot be made from these letters or is not in the dictionary. {guesses_remaining} guesses remaining.')
-            if not len(guessed_words):
-                print('You haven\'t entered any valid words')
-                return ""
-            else:
-                return max(guessed_words, key = len)
+        if not len(guessed_words):
+            print('You haven\'t entered any valid words')
+            return ""
+        else:
+            return max(guessed_words, key = len)
 
 ## Final Screen "You won/lost/drew _ to _ against the CPU"
     def round_result(self, player_total, computer_total):
@@ -112,17 +112,17 @@ class Anagram:
             print(f"You beat the CPU this round {player_total} to {computer_total}")
         elif player_total < computer_total:
             print(f'You lost to the CPU this round {player_total} to {computer_total}')
-        elif player_total == computer_total:
+        else:
             print(f'You drew with the CPU this round {player_total} to {computer_total}')
         
 
-    # def running_totals(self, player_total, computer_total):
-    #     player_running_total = 0
-    #     cpu_running_total = 0
-    #     for round in rounds:
-    #         player_running_total += player_total
-    #         cpu_running_total += computer_total
-    #     return player_running_total, cpu_running_total
+    def final_result(self, player_running_total, cpu_running_total):
+        if player_running_total > cpu_running_total:
+            print(f"You won! You beat the CPU {player_running_total} to {cpu_running_total} in a game of {self.rounds}")
+        elif player_running_total < cpu_running_total:
+            print(f'You lost! You were defeated by the CPU {player_running_total} to {cpu_running_total} in a game of {self.rounds}')
+        else:
+            print(f'A tie! You drew with the CPU {player_running_total} to {cpu_running_total} in a game of {self.rounds}')
 
 # Start function
 
@@ -138,12 +138,14 @@ class Anagram:
             print(f'The longest word possible is {len(longest_word)} letters.')
             guess = self.store_guesses(letters)
             # End of Round
-            print(f"Your best guess was: {guess}")
+            print(f"Your best guess was: '{guess}'")
             print(f"The computer's word was '{cpu_word}' for a score of {cpu_score} points.")
             print(f"The longest word was '{longest_word}'")
             self.round_result(len(guess), cpu_score)
             player_running_total += len(guess)
             cpu_running_total += cpu_score
-            
+        
+        self.final_result(player_running_total, cpu_running_total)
+        input("Press Enter to return to the Main Menu ")
             # Function for running total
             #print(f'At the end of round {} the score is Player: {player_running_total} to CPU: {cpu_running_total} ')
