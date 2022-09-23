c5547765955381c2c05d3b453dd0fa40f0899bef
JimmyNeutronDance <14wongom@gmail.com>
2022-09-23 23:00:43 +1000

    Whitespace enter error handling add

--- anagram.py
+++ anagram.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 from pick import pick
-from english_words import english_words_set
+from english_words import web2_lower_set as english_words_set
 import random
 import helper
 from letter_frequency import choose_consonant, choose_vowel
@@ -49,7 +49,6 @@ class Anagram:
                 if len(y) > 2 and y in english_words_set:
                     word_list.append(y)
         final_list = helper.remove_duplicates(word_list)
-        print(final_list)
         return final_list
 
 ## Function: Calculate CPU's length of word with word list.
@@ -61,26 +60,30 @@ class Anagram:
             cpu_word = final_list[int(round(len(final_list) * 0.65))]
             cpu_score = len(cpu_word)
         elif self.difficulty == 'Hard':
-            cpu_word = final_list[int(round(len(final_list) * 0.85))]
+            cpu_word = final_list[int(round(len(final_list) * 0.9))]
             cpu_score = len(cpu_word)
         return cpu_word, cpu_score
 
-    def validate_word(self, user_input, letters):
+    def validate_word(self, user_input, letters, guesses_remaining):
         # Check if user input is made up of letters in letters list
         lt_list = letters.copy()
+        if len(user_input) < 3:
+            if len(user_input):
+                print(f"Word too short, please enter a word of 3 letters or more. {guesses_remaining - 1} guesses remaining.")
+            return False
         if user_input in english_words_set:
             for letter in user_input:
                 if letter not in lt_list:
+                    print(f'{user_input} cannot be made from these letters. {guesses_remaining - 1} guesses remaining.')
                     return False
                 else:
                     lt_list.pop(lt_list.index(letter))
+            print(f"'{user_input}' is a valid word for a score of {len(user_input)}. {guesses_remaining - 1} guesses remaining.")
             return True
         else:
+            print(f"This word is not in the dictionary. {guesses_remaining - 1} guesses remaining.")
             return False
 
-
-    
-        
 
 
 # Function: Store input 3 times, tell user 3 guesses.
@@ -89,16 +92,19 @@ class Anagram:
         guessed_words = []
         while guesses_remaining > 0:
                 user_input = input("Enter your word: ")
-                if self.validate_word(user_input, letters):
+                user_input = user_input.strip()
+                if self.validate_word(user_input, letters, guesses_remaining):
                     guesses_remaining -= 1
                     guessed_words.append(user_input)
-                    print(f"'{user_input}' is a valid word for a score of {len(user_input)}. {guesses_remaining} guesses remaining.")
                 elif user_input == '':
-                    break
+                    if len(guessed_words) == 0:
+                        guesses_remaining -= 1
+                        print(f"You must make an guess to progress. {guesses_remaining} guesses remaining.")
+                        continue
+                    else:
+                        break
                 else:
                     guesses_remaining -= 1
-                    # TODO FIX ERROR HANDLING
-                    print(f'{user_input} cannot be made from these letters or is not in the dictionary. {guesses_remaining} guesses remaining.')
         if not len(guessed_words):
             print('You haven\'t entered any valid words')
             return ""
@@ -115,22 +121,22 @@ class Anagram:
         return guess, cpu_score
 
 
-    def round_result(self, player_total, computer_total):
+    def round_result(self, player_total, computer_total, rounds):
         if player_total > computer_total:
-            print(f'You beat the CPU {player_total} to {computer_total}')
+            print(f'You beat the CPU {player_total} to {computer_total} in Round {rounds}\n')
         elif player_total < computer_total:
-            print(f'You lost to the CPU {player_total} to {computer_total}')
+            print(f'You lost to the CPU {player_total} to {computer_total} in Round {rounds}\n')
         else:
-            print(f'You drew with the CPU {player_total} to {computer_total}')
+            print(f'You drew with the CPU {player_total} to {computer_total} in Round {rounds}\n')
         
 
     def final_result(self, player_running_total, cpu_running_total):
         if player_running_total > cpu_running_total:
-            print(f"You won! You beat the CPU {player_running_total} to {cpu_running_total} in a game of {self.rounds}")
+            print(f"You won! You beat the CPU {player_running_total} to {cpu_running_total} in a {self.rounds} round game.")
         elif player_running_total < cpu_running_total:
-            print(f'You lost! You were defeated by the CPU {player_running_total} to {cpu_running_total} in a game of {self.rounds}')
+            print(f'You lost! You were defeated by the CPU {player_running_total} to {cpu_running_total} in a {self.rounds} round game.')
         else:
-            print(f'A tie! You drew with the CPU {player_running_total} to {cpu_running_total} in a best of {self.rounds}')
+            print(f'A tie! You drew with the CPU {player_running_total} to {cpu_running_total} in a {self.rounds} round game.')
 
 # Start function
 
@@ -139,6 +145,7 @@ class Anagram:
         cpu_running_total = 0
         for i in range(self.rounds):
             print(f" \n***Round {i + 1}***\n")
+            print(f"The current score is Player: {player_running_total} CPU: {cpu_running_total}")
             letters = self.choose_letters()
             possible_words = self.possible_word_list(letters)
             cpu_word, cpu_score = self.cpu_score_calculator(possible_words)
@@ -150,11 +157,9 @@ class Anagram:
             guess, cpu_score = self.points_multipliers(guess, longest_word, cpu_score)
             print(f"\nThe computer's word was '{cpu_word}' for a score of {cpu_score} points.")
             print(f"\nThe longest word was '{longest_word}'\n")
-            self.round_result(len(guess), cpu_score)
-            print('\n')
+            self.round_result(len(guess), cpu_score, i + 1)
             player_running_total += len(guess)
             cpu_running_total += cpu_score
         self.final_result(player_running_total, cpu_running_total)
-        print('\n')
-        input("Press Enter to return to the Main Menu ")
+        input("\nPress Enter to return to the Main Menu ")
         os.system('clear')
